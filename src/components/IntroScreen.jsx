378ac95783719d016cbb9501f97c83363a3dadf6
IvernTheTree <90075556+IvernTheTree@users.noreply.github.com>
2022-07-22 03:05:01 +1200

    Initial commit

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
           </div>
           <section class="wrap">
           <Helmet>
-              <script type="module" src="./script.jsx" async></script>
+              <script type="module" src="../../src/components/script.jsx" async></script>
             </Helmet>
             <Link to="/music">
                 <button class="button">Start Listening</button>
