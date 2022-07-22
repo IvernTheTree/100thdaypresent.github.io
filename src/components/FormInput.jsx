d114ef43efdcacd5d66460dd9f942345b32f4b20
IvernTheTree <90075556+IvernTheTree@users.noreply.github.com>
2022-07-23 02:00:35 +1200

    I

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -7,10 +7,16 @@ const FormInput = (props) => {
     const handleFocus = (e) => {
         setFocused(true);
     };
-
+    function checking(){
+    alert ('hello')
+    }
+    function showHideButton(){
+        websiteButton = document.querySelector(".wrap");
+        websiteButton.classList.remove("hide");
+      }
     return(
         <div className="formInput">
-            <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()}/>
+            <input {...inputProps} onChange={onChange} onBlur={handleFocus} onBlurCapture={checking()} focused={focused.toString()}/>
             <span>{errorMessage}</span>
         </div>
     )
