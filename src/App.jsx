ef9c1491462dba16fcac67f5fab4fe13f6fe7aa0
IvernTheTree <90075556+IvernTheTree@users.noreply.github.com>
2022-07-24 22:15:34 +1200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -273,7 +273,7 @@ function App() {
               whileHover={{ scale: 1.09 }}
               whileTap={{ scale: 0.9 }}
               onClick={() => {
-                window.open("https://github.com/adamPatrick12");
+                window.open("https://github.com/IvernTheTree/IvernTheTree.github.io");
               }}
               className="link"
             >
